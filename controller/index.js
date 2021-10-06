6a2dd7dcf79c9c8a52ecacfcc15991407abf56ff
Alif Taufik <alif.tr.aminuddin@gmail.com>
2021-10-06 12:53:11 +0700

    Update MainController - Update data format matching test requirements

--- controller/index.js
+++ controller/index.js
@@ -2,19 +2,24 @@ const axios = require("axios");
 
 class MainController {
   static async getImages(req, res) {
-    const {
-      data: { items },
-    } = await axios.get(
+    const { data } = await axios.get(
       "https://www.flickr.com/services/feeds/photos_public.gne?format=json&nojsoncallback=1"
     );
 
-    const mappedData = items.map((item) => {
-      return {
-        image: item.media?.m ?? "",
-      };
-    });
+    const result = {
+      last_update: data.modified,
+      images: data.items.map((item) => {
+        const imageUrl = item.media?.m ?? "";
+        return {
+          title: item.title || imageUrl,
+          image_url: imageUrl,
+          flickr_link: item.link,
+          tags: item.tags.split(" "),
+        };
+      }),
+    };
 
-    res.json(mappedData);
+    res.json(result);
   }
 }
 
